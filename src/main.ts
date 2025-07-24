9413e93c0f6c32ec9803914a9f1be26580feb796
John-454 <chitancordobajohndeivid@gmail.com>
2025-07-23 22:50:48 -0500

    Fix: Register ionicons manually

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { environment } from './environments/environment';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-
-
 import { addIcons } from 'ionicons';
 import { receiptOutline, cubeOutline, searchOutline, cashOutline, addOutline, createOutline } from 'ionicons/icons';
 
@@ -17,6 +16,10 @@ addIcons({
   'add-outline': addOutline,
   'create-outline': createOutline,
 });
+
+if (environment.production) {
+  enableProdMode();
+}
 
 
 bootstrapApplication(AppComponent, {
